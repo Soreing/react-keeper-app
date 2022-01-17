4a366b319e44d1c181a24b102a1129cf3c05098d
Soreing <Soreing.dev@gmail.com>
2022-01-17 18:23:10 +0400

    Add authentication with Facebook

---
+++ Backend/controllers/auth/facebook-auth.js
@@ -0,0 +1,81 @@
+import axios from "axios";
+import ClientOAuth2 from "client-oauth2";
+import {User} from "../../database.js";
+import { redirectWithToken } from "../../helpers/common.js";
+
+const facebookAuth = new ClientOAuth2({
+    clientId: process.env.FACEBOOK_CLIENT_ID,
+    clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    accessTokenUri: "https://graph.facebook.com/v12.0/oauth/access_token",
+    authorizationUri: "https://www.facebook.com/v12.0/dialog/oauth",
+    redirectUri: "http://localhost:8081/auth/facebook/callback",
+    scopes: ["email", "public_profile"]
+});
+
+// Redirect the user to the Facebook OAuth2 URL
+exports.facebookAuth = (req, res, next) =>{
+    const uri = facebookAuth.code.getUri();
+    res.redirect(uri);
+}
+
+// Callback to the Google OAuth2 request
+exports.facebookCallback = (req, res, next) => {
+    // Exchanges the access code for a token
+    facebookAuth.code.getToken(req.originalUrl)
+    .then(function (user) {
+        // Requests information about the token (namely the user_id)
+        axios.request({
+            baseURL: "https://graph.facebook.com",
+            url: "/debug_token",
+            method: "get",
+            params: {
+                input_token: user.accessToken,
+                access_token: `${process.env.FACEBOOK_CLIENT_ID}|${process.env.FACEBOOK_CLIENT_SECRET}`,
+            }
+        }) // token.data.data => {user_id}
+        .then((token)=>{
+            // Requests more information about the user with the user_id
+            axios.request({
+                baseURL: "https://graph.facebook.com",
+                url: `/v12.0/${token.data.data.user_id}/`,
+                method: "get",
+                headers:{
+                    "Authorization": `Bearer ${user.accessToken}`
+                }
+            }) // details.data => {name, id}
+            .then((details)=>{
+                const userQuery = {
+                    username: `facebook:${details.data.id}`,
+                    source: "facebook",
+                };
+                const options = {
+                    upsert: true,
+                    returnDocument:"after",
+                }
+    
+                // Find or Insert the user in the database by their username ("facebook:facebookID")
+                User.findOneAndUpdate(userQuery, {}, options, (findErr, record)=>{
+                    if(!findErr && record){
+                        redirectWithToken(res, record._id.toString());
+                    }
+                    else {
+                        console.log(findErr);
+                        res.sendStatus(500);
+                    }
+                });
+            })
+            .catch((err)=>{
+                console.log(err);
+                res.sendStatus(500);
+            });
+        })
+        .catch((err)=>{
+            console.log(err);
+            res.sendStatus(500);
+        });
+    })
+    // The user likely rejected the 3rd Party authentication
+    .catch((err)=>{
+        res.redirect("http://localhost:8080/login");
+    })
+}
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,7 @@ import express from "express";
 import {logout, refreshAuthToken} from "../controllers/auth/common-auth.js";
 import {login, register, verify} from "../controllers/auth/local-auth.js";
 import {googleAuth, googleCallback} from "../controllers/auth/google-auth.js";
+import {facebookAuth, facebookCallback} from "../controllers/auth/facebook-auth.js";
 import {discordAuth, discordCallback} from "../controllers/auth/discord-auth.js";
 
 const router = express.Router();
@@ -15,6 +16,9 @@ router.get("/auth/verify/:verCode", verify);
 
 router.get("/auth/google", googleAuth);
 router.get("/auth/google/callback", googleCallback);
+
+router.get("/auth/facebook", facebookAuth);
+router.get("/auth/facebook/callback", facebookCallback);
 
 router.get("/auth/discord", discordAuth);
 router.get("/auth/discord/callback", discordCallback);
